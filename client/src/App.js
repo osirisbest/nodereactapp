0fe3bfafa5f3698a51edd1f8692d2472cac53244
DESKTOP-ENJJ32B\os <osirisbest@gmail.com>
2018-06-19 09:37:52 +0300

    file

--- client/src/App.js
+++ client/src/App.js
@@ -9,12 +9,14 @@ class App extends Component {
 
   componentDidMount() {
     this.callApi()
-      .then(res => this.setState({ response: res.express }))
+      .then(res => this.setState({ response: res.data }))
+      .then(res=>console.log(res.data))
       .catch(err => console.log(err));
   }
 
   callApi = async () => {
     const response = await fetch('/api/hello');
+    console.log(response)
     const body = await response.json();
 
     if (response.status !== 200) throw Error(body.message);
@@ -30,7 +32,7 @@ class App extends Component {
           <h1 className="App-title">This is react with gate to NodeJS Express</h1>
         </header>
         <p className="App-intro">
-        {this.state.response}
+        { this.state.response }
         </p>
       </div>
     );
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const fs = require("fs");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+    fs.readFile("data.txt","utf8", function (err, data) {
+        if (err) throw err
+        console.log(data);
+        res.send({data});
+    });
+// res.send({ data: 'Hello From Express' }); test string!
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
