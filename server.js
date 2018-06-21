1be1e962f6bc0534ae2a6f0bf8c54a303b1ca03a
osirisbest <osirisbest@gmail.com>
2018-06-21 20:07:53 +0300

    format

--- server.js
+++ server.js
@@ -9,20 +9,20 @@ app.use(express.json())
 const port = process.env.PORT || 5000
 
 app.get('/api/getData', (req, res) => {
-    fs.readFile("data.txt","utf8", function (err, data) {
+    fs.readFile("data.txt", "utf8", function (err, data) {
         if (err) throw err
         console.log(data)
-        res.send({data})
+        res.send({ data })
     });
 });
 
-var urlencodedParser = bodyParser.urlencoded({extended: false});
-app.post('/api/putData',urlencodedParser, function(req,res){
-    if(!req.body) return response.sendStatus(400)
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.post('/api/putData', urlencodedParser, function (req, res) {
+    if (!req.body) return response.sendStatus(400)
     res.send(req.body)
-    console.log( req.body)
-    fs.writeFile("data.txt",(req.body.data),function(error){
-        if(error) throw error
+    console.log(req.body)
+    fs.writeFile("data.txt", (req.body.data), function (error) {
+        if (error) throw error
     })
 })
 
